5ad356410bb1f8f2e16bc195bb5c2c6a68698b42
Arsen <xakars02@gmail.com>
2022-11-30 11:50:58 +0600

    add Place model

--- places/admin.py
+++ places/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from places.models import Place
+
+admin.site.register(Place)
--- places/models.py
+++ places/models.py
@@ -1,4 +1,12 @@
 from django.db import models
 
-# Create your models here.
-print("Hi")
+
+class Place(models.Model):
+    title = models.CharField("Название локации", max_length=200)
+    description_short = models.CharField("Краткое описание", max_length=600, blank=True)
+    description_long = models.TextField("Подробное описание", blank=True)
+    lat = models.FloatField('Широта', blank=True)
+    lon = models.FloatField('Долгота', blank=True)
+
+    def __str__(self):
+        return self.title
