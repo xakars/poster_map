aa96f20508b81cef922f1c54bd84e00735fb5b58
Arsen <xakars02@gmail.com>
2022-12-04 15:24:23 +0600

    fix STATICFILES_DIRS setting should not contain the STATIC_ROOT

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -124,12 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = os.getenv('STATIC_URL', '/static/')
+STATIC_ROOT = '/static/'
 
-if DEBUG:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static')]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
