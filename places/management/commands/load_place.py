2f46a7d42d81fbbd44b797d5be5e4bc7e08eb6c5
Arsen <xakars02@gmail.com>
2022-12-21 15:14:24 +0600

    fix getting value from dict

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -10,26 +10,30 @@ class Command(BaseCommand):
         parser.add_argument('url', type=str)
 
     def handle(self, *args, **options):
-        url = options['url']
-        response = requests.get(url)
-        response.raise_for_status()
-        place_detail = response.json()
-
-        place, created = Place.objects.get_or_create(
-            title=place_detail['title'],
-            lng=place_detail['coordinates']['lng'],
-            lat=place_detail['coordinates']['lat'],
-            defaults= {
-                'description_short': place_detail['description_short'],
-                'description_long': place_detail['description_long'],
-            }
-        )
-        if not created:
-            place.imgs.all().delete()
-
-        img_urls = place_detail['imgs']
-        for index, url in enumerate(img_urls):
-            img_response = requests.get(url)
+        try:
+            url = options['url']
+            response = requests.get(url)
             response.raise_for_status()
-            content = ContentFile(img_response.content, name=f'{str(index)}.jpg')
-            Image.objects.create(place=place, img=content, position=index)
+            place_detail = response.json()
+
+            place, created = Place.objects.get_or_create(
+                title=place_detail['title'],
+                lng=place_detail['coordinates']['lng'],
+                lat=place_detail['coordinates']['lat'],
+                defaults= {
+                    'description_short': place_detail.get('description_short', ''),
+                    'description_long': place_detail.get('description_long', '')
+                }
+            )
+            if not created:
+                place.imgs.all().delete()
+
+            img_urls = place_detail.get('imgs', [])
+            for index, url in enumerate(img_urls):
+                img_response = requests.get(url)
+                response.raise_for_status()
+                content = ContentFile(img_response.content, name=f'{str(index)}.jpg')
+                Image.objects.create(place=place, img=content, position=index)
+
+        except KeyError as e:
+               print(f'В json-е локации не найдено значение {e}')
